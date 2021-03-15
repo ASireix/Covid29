e8811ce26e252d37edb9447c62998a8471119cf8
ASireix <60692336+ASireix@users.noreply.github.com>
2021-03-15 15:45:40 +0100

    json,node,cluster

--- npm/nodeApp.js
+++ npm/nodeApp.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express();
 const http = require('http').createServer(app);
 
+app.use('/ressources', express.static('static'))
+app.use('/cluster', express.static('node_modules/leaflet.markercluster/dist'))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static/carte.html');
 });
